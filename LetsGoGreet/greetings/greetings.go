2c0ac1e49c74706c5e7f284024b7a61be7f3bb2f
Joao Favero <jvfavero@gmail.com>
2024-06-01 15:34:56 -0300

    Tutorial: Create a module

--- LetsGoGreet/greetings/greetings.go
+++ LetsGoGreet/greetings/greetings.go
@@ -14,9 +14,9 @@ func Hello(name string) (string, error) {
     }
     // Create a message using a random format.
     message := fmt.Sprintf(randomFormat(), name)
+    //message := fmt.Sprint(randomFormat())
     return message, nil
 }
-
 // Hellos returns a map that associates each of the named people
 // with a greeting message.
 func Hellos(names []string) (map[string]string, error) {
